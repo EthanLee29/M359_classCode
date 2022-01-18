1e87b772a22a8f7d5c9542f05e9509676d6a2c42
EthanLee29 <90778091+EthanLee29@users.noreply.github.com>
2022-01-18 10:12:41 -0600

    1/18/22

---
+++ M359/src/U7_ArrayLists/lab/TicketMasterDriver.java
@@ -0,0 +1,14 @@
+package U7_ArrayLists.lab;
+
+public class TicketMasterDriver {
+    public static int getUserChoice(Scanner input) {
+        /*
+        ask the user for their choice
+        use try block to read their choice
+            if the choice is an int, then make sure its in the right range
+            if its not an int, and goes to catch, then give user a message
+
+         after you have a good choice, in desired range, return this value
+         */
+    }
+}
